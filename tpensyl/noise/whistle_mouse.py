c13e21c81619988e0cfe6ff39594647b84ec45a0
Tommy Pensyl <tommy.pensyl@gmail.com>
2022-12-11 19:59:58 -0500

    relative whistle pitch

--- tpensyl/noise/whistle_mouse.py
+++ tpensyl/noise/whistle_mouse.py
@@ -12,24 +12,30 @@ class WhistleActions:
     def whistle_start(power:float, f0:float, f1:float, f2:float):
         """for debugging"""
         print("whistle start",[int(x) for x in (power, f0, f1, f2)])
-        global base1, frame
-        base = f1
-        frame = 0
+        global start_frames
+        f = f0
+        start_frames = [f]
 
     def whistle_stop(power:float, f0:float, f1:float, f2:float):
         """for debugging"""
         print("whistle stop",[int(x) for x in (power, f0, f1, f2)])
-        global base
+        global base 
         
 
     def whistle_repeat(power:float, f0:float, f1:float, f2:float): 
         """for debugging"""
-        #print("whistle", [int(x) for x in (power, f0, f1, f2)])
-        global base
-        delta =  f1 - base  
-        if abs(delta) > .5:
-            print(delta)
+        global start_frames
+        f = f0
+        if len(start_frames) < 5:
+            start_frames += [f]
+
+        base = sum(start_frames) / len(start_frames)
+        
+        delta =  f - base  
+        if abs(delta) > 0: 
+            #print(delta)
 
             mouse_pos = ctrl.mouse_pos()
-            print( mouse_pos )
+            #print( mouse_pos )
             ctrl.mouse_move(mouse_pos[0] + delta*speed_scaler, mouse_pos[1])
+        print("whistle", [int(x) for x in [power, f0, f1, f2, f]+start_frames])
