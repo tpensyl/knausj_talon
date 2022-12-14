7ae0431e8258edfc8250c30bc600fd92d42d071a
Tommy Pensyl <tommy.pensyl@gmail.com>
2022-12-14 18:59:50 -0500

    parrot and whistle

--- tpensyl/noise/whistle_mouse.py
+++ tpensyl/noise/whistle_mouse.py
@@ -1,35 +1,51 @@
-from talon import Module, actions, noise, ctrl
+from talon import Module, Context, actions, noise, ctrl
 
+ctx = Context()
+ctx.matches = r"""
+app.name: disable
+"""
 
 mod = Module()
 
 delta_threshold = 100
 speed_scaler = .1
+pause_threshold = .3
+static_base = 650
+
+start_frames = []
+ts = 0
 
 @mod.action_class
 class WhistleActions:
 
-    def whistle_start(power:float, f0:float, f1:float, f2:float):
+    def whistle_start(ts:float, power:float, f0:float, f1:float, f2:float):
         """for debugging"""
-        print("whistle start",[int(x) for x in (power, f0, f1, f2)])
-        global start_frames
+        print("whistle start",[int(x) for x in (10*ts, power, f0, f1, f2)])
+        global start_frames, stop_ts
+        if ts - stop_ts < pause_threshold:
+            print("continue")
+            return
         f = f0
         start_frames = [f]
 
-    def whistle_stop(power:float, f0:float, f1:float, f2:float):
+    def whistle_stop(ts:float, power:float, f0:float, f1:float, f2:float):
         """for debugging"""
-        print("whistle stop",[int(x) for x in (power, f0, f1, f2)])
-        global base 
+        print("whistle stop",[int(x) for x in (10*ts, power, f0, f1, f2)])
+        global stop_ts
+        stop_ts = ts 
         
 
-    def whistle_repeat(power:float, f0:float, f1:float, f2:float): 
+    def whistle_repeat(ts:float, power:float, f0:float, f1:float, f2:float): 
         """for debugging"""
-        global start_frames
+        global start_frames 
         f = f0
         if len(start_frames) < 5:
             start_frames += [f]
 
-        base = sum(start_frames) / len(start_frames)
+        if static_base:
+            base = static_base
+        else:
+            base = sum(start_frames) / len(start_frames)
         
         delta =  f - base  
         if abs(delta) > 0: 
@@ -38,4 +54,4 @@ class WhistleActions:
             mouse_pos = ctrl.mouse_pos()
             #print( mouse_pos )
             ctrl.mouse_move(mouse_pos[0] + delta*speed_scaler, mouse_pos[1])
-        print("whistle", [int(x) for x in [power, f0, f1, f2, f]+start_frames])
+        print("whistle", [int(x) for x in [power, f0, f1, f2, f]])
